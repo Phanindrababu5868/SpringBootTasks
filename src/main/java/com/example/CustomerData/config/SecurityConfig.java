672fb23278d046fcbae1cadfb7ce69488f73019b
phanindra <chandraphanindrababu@gmail.com>
2022-08-08 19:07:43 +0530

    Hotel Management rolebasedAuthorization

--- src/main/java/com/example/CustomerData/config/SecurityConfig.java
+++ src/main/java/com/example/CustomerData/config/SecurityConfig.java
@@ -3,10 +3,12 @@ import com.example.CustomerData.Security.CustomerDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,7 +22,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private CustomerDetails Customerdata;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
+        http .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable().httpBasic().and().authorizeRequests()
+                .antMatchers("/api/CustomerData/**").hasAnyAuthority("admin")
+                .antMatchers(HttpMethod.GET,"/api/Customer").hasAnyAuthority("Receptionist")
+                .anyRequest().authenticated();
+
+
+
     }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
---
+++ src/main/java/com/example/CustomerData/config/base.java
@@ -0,0 +1,56 @@
+//package com.example.CustomerData.config;
+//package com.prem.hotelmanagement.api.config;
+//
+//import com.prem.hotelmanagement.api.Security.EmployeeDetails;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class AuthConfig extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    EmployeeDetails employeeDetails;
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf()
+//                .disable()
+//
+//                .httpBasic()
+//
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/help","/help/**").permitAll()
+//                .antMatchers(HttpMethod.GET,"/rooms","/rooms/**").permitAll()
+//
+//                .antMatchers("/admin/**").hasAnyAuthority("ADMIN")
+//                .antMatchers(HttpMethod.POST,"/rooms").hasAnyAuthority("ADMIN")
+//
+//                .antMatchers("/user","/user/**").hasAnyAuthority( "RECEPTIONIST")
+//                .antMatchers("/bookings","/bookings/*").hasAnyAuthority( "RECEPTIONIST")
+//                .anyRequest().authenticated()
+//
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(this.employeeDetails).passwordEncoder(passwordEncoder());
+//    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//}
